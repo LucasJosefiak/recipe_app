48884ecead088ee6608478716869eba2fa6007b4
Lucas Josefiak <lucas@firecrown.pro>
2021-10-04 18:05:28 +0200

    tested storybook

--- stories/main.dart
+++ stories/main.dart
@@ -1,21 +1,56 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:groceries_app/models/recipe.dart';
 import 'package:groceries_app/providers/firebase_setup_provider.dart';
+import 'package:groceries_app/widgets/buttons/custom_elevated_button.dart';
+import 'package:groceries_app/widgets/ingredient_info.dart';
+import 'package:groceries_app/widgets/recipe_info.dart';
 import 'package:provider/provider.dart';
+import 'package:storybook/storybook.dart';
 
 import 'storybook.dart';
 
 void main() {
-  Provider.debugCheckInvalidValueType = null;
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => FirebaseSetupProvider()..initializeFirebase(),
-          //TODO @Jens: What does ".." do here. I know that it's the cascade operator but in this case I don't get it.
+    storyBookHome(
+      logo: Container(
+        child: Text(
+          'Storybook',
+          style: TextStyle(color: Colors.black, fontSize: 20),
+        ),
+      ),
+      listThemeData: [
+        {'Theme': ThemeData()},
+        {'Theme dark': ThemeData.dark()}
+      ],
+      listStoryBookModel: [
+        StoryBookModel(
+          storyTitle: 'ElevatedButton',
+          storyDescription: 'Test',
+          story: StoryElementModel(
+            builder: (context) => Elevated(
+              function: null,
+              text: 'Hello',
+            ),
+          ),
+          sourceStoryFilePath:
+              'lib/widgets/buttons/custom_elevated_button.dart',
+        ),
+        StoryBookModel(
+          storyTitle: 'RecipeInfo',
+          storyDescription: 'Test',
+          story: StoryElementModel(
+            builder: (context) => RecipeInfo(
+              recipe: Recipe(
+                title: 'Recipe',
+                createdAt: DateTime.now(),
+              ),
+            ),
+          ),
+          sourceStoryFilePath:
+              'lib/widgets/buttons/custom_elevated_button.dart',
         ),
       ],
-      child: Storybook(),
     ),
   );
 }
