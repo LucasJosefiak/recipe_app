c8c36a8aa3bc414620ab81afe5dcfbb0df78bd9d
Lucas Josefiak <lucas@firecrown.pro>
2021-11-07 18:49:32 +0100

    fixed issue for iPhone 7

--- lib/widgets/recipe_placeholder.dart
+++ lib/widgets/recipe_placeholder.dart
@@ -9,24 +9,24 @@ class RecipePlaceholder extends StatelessWidget {
   Widget build(BuildContext context) {
     return Transform.scale(
       scale: 0.8,
-      child: Transform.rotate(
-        angle: -radians(2),
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: Radii.cardRadius,
-              child: Image.asset('assets/recipe-stock-image.jpg'),
-            ),
-            SizedBox(
-              height: 16,
-            ),
+      // child: Transform.rotate(
+      //   // angle: -radians(2),
+      //   child: Column(
+      child: Center(
+        child:
+            // ClipRRect(
+            //   borderRadius: Radii.cardRadius,
+            //   child: Image.asset('assets/recipe-stock-image.jpg'),
+            // ),
+            // SizedBox(
+            //   height: 16,
+            // ),
             Text(
-              'Please add your favorite recipes!',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-          ],
+          'Please add your favorite recipes!',
+          style: Theme.of(context).textTheme.headline6,
         ),
       ),
+      // ),
     );
   }
 }
